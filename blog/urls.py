aba195bb24125581a046bb2a3ccc5c9f8136fa2b
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-10-24 17:55:09 +0000

    Link url to post_detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from . import views
 
+"""
+Note that some code was used from Code Institute's 'I think therefore I blog'
+tutorial to help create this project.
+"""
+
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.Postpagination.as_view(), name="home"),
+    path('<slug:slug>/', views.PostDetail.as_view(), name="post_detail"),
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about"),
 ]
