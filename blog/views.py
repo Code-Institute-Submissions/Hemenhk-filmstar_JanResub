51fc24ae7e27a92a25cf9f9077294d143f361464
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-10-24 17:51:28 +0000

    Create post_detail.html file

--- blog/views.py
+++ blog/views.py
@@ -4,11 +4,43 @@ from django.views import generic, View
 from .models import Post
 
 
+"""
+Note that code was used here from Code Institute's "I think therefore I blog
+project" tutorial to help create this project
+"""
+
+
 class Postpagination(generic.ListView):
+    """
+    View for the post detail
+    """
     model = Post
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
     paginate_by = 6
+
+
+class PostDetail(View):
+    """
+    View for the post detail
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
 
 
 def home(request):
