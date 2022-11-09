559b8e40f2af35df40088fbe324c17de538715d2
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-11-09 17:58:15 +0000

    Add view for PostDetail

--- blog/views.py
+++ blog/views.py
@@ -114,3 +114,25 @@ def contact(request):
 def about(request):
     return render(request, "about.html")
 
+
+class PostDetail(View):
+    """
+    View for post details
+    """
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "liked": liked
+            }
+        )
