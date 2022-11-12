d72a65dc2b75217482fac3e91ae3ca32fc7502ff
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-11-12 21:54:09 +0000

    Create test for contact url

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
-from blog.views import PostList, PostLike, PostDetail
+from blog.views import PostList, PostLike, PostDetail, contact, about
 
 """
 This code was inspired by The Dumbfounds. Specific link can be found in the readme.
@@ -20,3 +20,7 @@ class TestUrls(SimpleTestCase):
     def test_post_like_url_is_resolved(self):
         url = reverse('post_like', args=['test-slug'])
         self.assertEquals(resolve(url).func.view_class, PostLike)
+
+    def test_post_contact_is_resolved(self):
+        url = reverse('contact')
+        self.assertEquals(resolve(url).func, contact)
