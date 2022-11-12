1e5d6fc6623caa99d227533ba0ba08c77109a84e
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-11-12 20:21:31 +0000

    Create test for CommentForm

--- blog/tests/test_forms.py
+++ blog/tests/test_forms.py
@@ -2,10 +2,14 @@ from django.test import TestCase
 from blog.forms import CommentForm
 
 
-class TestForms(TestCase):
+class TestCommentForm(TestCase):
 
     def test_comment_bodyfield_is_required(self):
         form = CommentForm({'body': ''})
         self.assertFalse(form.is_valid())
-        self.assertIn('name', form.errors.keys())
+        self.assertIn('body', form.errors.keys())
         self.assertEqual(form.errors['body'][0], 'This field is required.')
+
+    def test_fields_are_explicit_in_form_metaclass(self):
+        form = CommentForm()
+        self.assertEqual(form.Meta.fields, ('body', ))
