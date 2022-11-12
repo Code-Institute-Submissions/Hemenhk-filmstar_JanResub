ddd51085c3d052dc5bb558edf2d16b8f8cae0868
Hemenhk <hemen.hiwakamal@hotmail.com>
2022-11-12 21:51:25 +0000

    Create test for PostDetail url

--- blog/tests/test_urls.py
+++ blog/tests/test_urls.py
@@ -12,3 +12,7 @@ class TestUrls(SimpleTestCase):
     def test_home_url_is_resolved(self):
         url = reverse('home')
         self.assertEquals(resolve(url).func.view_class, PostList)
+
+    def test_post_detail_url_is_resolved(self):
+        url = reverse('post_detail', args=['test-slug'])
+        self.assertEquals(resolve(url).func.view_class, PostDetail)
